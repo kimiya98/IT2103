ebc7fa1ae636599f5135de28d00c2fa9afef766f
kimiya98 <kittyap12@gmail.com>
2016-09-27 14:18:20 +0800

    Added a glyphicon

--- home.php
+++ home.php
@@ -3,6 +3,6 @@
 	<link rel='stylesheet' href='css/bootstrap.min.css'>
 </head>
 <body>
-	<button class='btn btn-danger'>NO</button>
+	<button class='btn btn-danger'><span class = 'glyphicon glyphicon-user'>NO</span></button>
 </body>
 </html>
